dd08b07f4004ba7c7094d13700c73588d51cd57b
Marcos Barbero <marcos.hgb@gmail.com>
2016-12-21 16:22:33 +0100

    Fix to work on redis-cluster

--- src/main/java/com/marcosbarbero/zuul/filters/pre/ratelimit/config/redis/RedisRateLimiter.java
+++ src/main/java/com/marcosbarbero/zuul/filters/pre/ratelimit/config/redis/RedisRateLimiter.java
@@ -3,13 +3,10 @@ package com.marcosbarbero.zuul.filters.pre.ratelimit.config.redis;
 import com.marcosbarbero.zuul.filters.pre.ratelimit.config.Policy;
 import com.marcosbarbero.zuul.filters.pre.ratelimit.config.Rate;
 import com.marcosbarbero.zuul.filters.pre.ratelimit.config.RateLimiter;
-import org.springframework.dao.DataAccessException;
-import org.springframework.data.redis.core.RedisOperations;
-import org.springframework.data.redis.core.RedisTemplate;
-import org.springframework.data.redis.core.SessionCallback;
 
-import java.util.List;
-import java.util.concurrent.TimeUnit;
+import org.springframework.data.redis.core.RedisTemplate;
+
+import static java.util.concurrent.TimeUnit.SECONDS;
 
 /**
  * @author Marcos Barbero
@@ -24,19 +21,14 @@ public class RedisRateLimiter implements RateLimiter {
     @Override
     @SuppressWarnings("unchecked")
     public Rate consume(final Policy policy, final String key) {
-        Long now = System.currentTimeMillis();
-        Long time = (now / (1000 * policy.getRefreshInterval()));
-        final String tempKey = key + ":" + time;
-        List results = (List) template.execute(new SessionCallback() {
-            @Override
-            public List<Object> execute(RedisOperations ops) throws DataAccessException {
-                ops.multi();
-                ops.boundValueOps(tempKey).increment(1L);
-                ops.expire(tempKey, policy.getRefreshInterval(), TimeUnit.SECONDS);
-                return ops.exec();
-            }
-        });
-        Long current = (Long) results.get(0);
-        return new Rate(policy.getLimit(), Math.max(-1, policy.getLimit() - current), time * (policy.getRefreshInterval() * 1000));
+        final Long limit = policy.getLimit();
+        final Long refreshInterval = policy.getRefreshInterval();
+        final Long current = this.template.boundValueOps(key).increment(1L);
+        Long expire = this.template.getExpire(key);
+        if (expire == null || expire == -1) {
+            this.template.expire(key, refreshInterval, SECONDS);
+            expire = refreshInterval;
+        }
+        return new Rate(limit, Math.max(-1, limit - current), SECONDS.toMillis(expire));
     }
 }
