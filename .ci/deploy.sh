f6fa949d57709d218dbb647c5e112ec2fffb0ec5
Marcos Barbero <marcos.hgb@gmail.com>
2018-04-18 15:41:14 +0200

    [ci deploy] fix deployment script

--- .ci/deploy.sh
+++ .ci/deploy.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 
 if [[ $TRAVIS_COMMIT_MESSAGE == *"[ci deploy]"* ]]; then
-  mvn --settings ./settings.xml -B -f ../pom.xml clean deploy -Pdeploy-parent -N
-  mvn --settings ./settings.xml -B -f ../pom.xml clean deploy -Pdeploy -N
+  mvn --settings .ci/settings.xml clean deploy -Pdeploy-parent -N
+  mvn --settings .ci/settings.xml clean deploy -Pdeploy -N
 fi
