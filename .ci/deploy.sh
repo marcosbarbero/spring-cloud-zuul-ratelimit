6aee4f877d7bae52760b38c7d755f079e2fc8b21
Marcos Barbero <marcos.hgb@gmail.com>
2018-04-21 14:41:18 +0200

    Fix travis+maven deploy integration

--- .ci/deploy.sh
+++ .ci/deploy.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 
-if [[ $TRAVIS_COMMIT_MESSAGE == *"[ci deploy]"* ]]; then
+if [[ "$TRAVIS_BRANCH" = 'master' ] && [ "$TRAVIS_PULL_REQUEST" == 'false' && $TRAVIS_COMMIT_MESSAGE == *"[ci deploy]"* ]]; then
   mvn --settings .ci/settings.xml clean deploy -DskipTests -Pdeploy-parent -N
   mvn --settings .ci/settings.xml -f spring-cloud-zuul-ratelimit-dependencies/pom.xml clean deploy -DskipTests -Pdeploy
   mvn --settings .ci/settings.xml -f spring-cloud-starter-zuul-ratelimit/pom.xml clean deploy -DskipTests -Pdeploy
