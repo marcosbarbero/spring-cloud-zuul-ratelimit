c8147e561efe1efba82e8538b3c6db4e5e8e7bd7
Marcos Barbero <marcos.hgb@gmail.com>
2018-04-28 10:49:16 +0200

    Fix deploy conditionals

--- .ci/deploy.sh
+++ .ci/deploy.sh
@@ -1,8 +1,12 @@
 #!/usr/bin/env bash
 
-if [[ "$TRAVIS_BRANCH" = 'master' ] && [ "$TRAVIS_PULL_REQUEST" == 'false' ] && [ $TRAVIS_COMMIT_MESSAGE == *"[ci deploy]"* ]]; then
+if [ "$TRAVIS_BRANCH" = 'master' ] && [ "$TRAVIS_PULL_REQUEST" == 'false' ] && [ "$TRAVIS_COMMIT_MESSAGE" == *"[ci deploy]"* ]; then
   mvn --settings .ci/settings.xml clean deploy -DskipTests -Pdeploy-parent -N
   mvn --settings .ci/settings.xml -f spring-cloud-zuul-ratelimit-dependencies/pom.xml clean deploy -DskipTests -Pdeploy
   mvn --settings .ci/settings.xml -f spring-cloud-starter-zuul-ratelimit/pom.xml clean deploy -DskipTests -Pdeploy
   mvn --settings .ci/settings.xml -f spring-cloud-zuul-ratelimit-core/pom.xml clean deploy -DskipTests -Pdeploy
+
+  echo "New version released"
 fi
+
+echo "Build Finished"
