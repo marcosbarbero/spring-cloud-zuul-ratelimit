0053db559453ce299038f7ca0b2e3d4435d847a7
Marcos Barbero <marcos.hgb@gmail.com>
2018-05-02 11:45:03 +0200

    Improve [ci deploy] script

--- .ci/deploy.sh
+++ .ci/deploy.sh
@@ -1,5 +1,9 @@
 #!/usr/bin/env bash
 
+echo "Branch: $TRAVIS_BRANCH"
+echo "Pull Request? $TRAVIS_PULL_REQUEST"
+echo "Commit Message: $TRAVIS_COMMIT_MESSAGE"
+
 if [ "$TRAVIS_BRANCH" = 'master' ] && [ "$TRAVIS_PULL_REQUEST" == 'false' ] && [ "$TRAVIS_COMMIT_MESSAGE" == *"[ci deploy]"* ]; then
   mvn --settings .ci/settings.xml clean deploy -DskipTests -Pdeploy-parent -N
   mvn --settings .ci/settings.xml -f spring-cloud-zuul-ratelimit-dependencies/pom.xml clean deploy -DskipTests -Pdeploy
