fd2deb5c7723339d3cdc42f034486641fde377ed
Liel Chayoun <lchayoun@users.noreply.github.com>
2018-10-30 13:47:02 +0100

    Update spring-cloud-zuul-ratelimit-core/src/main/java/com/marcosbarbero/cloud/autoconfigure/zuul/ratelimit/config/properties/validators/PoliciesValidator.java
    Co-Authored-By: marcosbarbero <marcos.hgb@gmail.com>

--- spring-cloud-zuul-ratelimit-core/src/main/java/com/marcosbarbero/cloud/autoconfigure/zuul/ratelimit/config/properties/validators/PoliciesValidator.java
+++ spring-cloud-zuul-ratelimit-core/src/main/java/com/marcosbarbero/cloud/autoconfigure/zuul/ratelimit/config/properties/validators/PoliciesValidator.java
@@ -68,7 +68,7 @@ public class PoliciesValidator implements ConstraintValidator<Policies, Object> 
     }
 
     private boolean isValidRoles(Policy policy) {
-        return policy.getType().stream()
+        return policy.getType().stream().noneMatch((type -> type.getType().equals(ROLE) && type.getMatcher() == null);
                 .filter(type -> type.getType().equals(ROLE))
                 .filter(type -> type.getMatcher() == null)
                 .collect(toList()).isEmpty();
