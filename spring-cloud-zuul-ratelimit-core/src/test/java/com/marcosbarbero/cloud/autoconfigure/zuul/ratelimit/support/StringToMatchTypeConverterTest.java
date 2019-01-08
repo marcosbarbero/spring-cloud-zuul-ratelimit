6b24c78b0be1b4403b8abbb8516fdc864c19c6fd
XieEDeHeiShou@xgtl <2325690622@qq.com>
2019-01-08 15:06:44 +0800

    Test RateLimitType.METHOD in StringToMatchTypeConverterTest

--- spring-cloud-zuul-ratelimit-core/src/test/java/com/marcosbarbero/cloud/autoconfigure/zuul/ratelimit/support/StringToMatchTypeConverterTest.java
+++ spring-cloud-zuul-ratelimit-core/src/test/java/com/marcosbarbero/cloud/autoconfigure/zuul/ratelimit/support/StringToMatchTypeConverterTest.java
@@ -1,11 +1,11 @@
 package com.marcosbarbero.cloud.autoconfigure.zuul.ratelimit.support;
 
-import static org.assertj.core.api.Assertions.assertThat;
-
 import com.marcosbarbero.cloud.autoconfigure.zuul.ratelimit.config.properties.RateLimitProperties.Policy.MatchType;
 import com.marcosbarbero.cloud.autoconfigure.zuul.ratelimit.config.properties.RateLimitType;
 import org.junit.Before;
 import org.junit.Test;
+
+import static org.assertj.core.api.Assertions.assertThat;
 
 public class StringToMatchTypeConverterTest {
 
@@ -19,6 +19,7 @@ public class StringToMatchTypeConverterTest {
     @Test
     public void testConvertStringTypeOnly() {
         MatchType matchType = target.convert("url");
+        assertThat(matchType).isNotNull();
         assertThat(matchType.getType()).isEqualByComparingTo(RateLimitType.URL);
         assertThat(matchType.getMatcher()).isNull();
     }
@@ -26,7 +27,24 @@ public class StringToMatchTypeConverterTest {
     @Test
     public void testConvertStringTypeWithMatcher() {
         MatchType matchType = target.convert("url=/api");
+        assertThat(matchType).isNotNull();
         assertThat(matchType.getType()).isEqualByComparingTo(RateLimitType.URL);
         assertThat(matchType.getMatcher()).isEqualTo("/api");
+    }
+
+    @Test
+    public void testConvertStringTypeMethodOnly() {
+        MatchType matchType = target.convert("method");
+        assertThat(matchType).isNotNull();
+        assertThat(matchType.getType()).isEqualByComparingTo(RateLimitType.METHOD);
+        assertThat(matchType.getMatcher()).isNull();
+    }
+
+    @Test
+    public void testConvertStringTypeMethodWithMatcher() {
+        MatchType matchType = target.convert("method=get");
+        assertThat(matchType).isNotNull();
+        assertThat(matchType.getType()).isEqualByComparingTo(RateLimitType.METHOD);
+        assertThat(matchType.getMatcher()).isEqualTo("get");
     }
 }
