45f098cb4cefbbc853927916c2275292267a84d2
Marcos Barbero <marcos.hgb@gmail.com>
2019-01-13 19:21:13 +0100

    Change [ci deploy] file

--- .ci/deploy.sh
+++ .ci/deploy.sh
@@ -4,7 +4,7 @@ echo "Branch: $TRAVIS_BRANCH"
 echo "Pull Request? $TRAVIS_PULL_REQUEST"
 echo "Commit Message: $TRAVIS_COMMIT_MESSAGE"
 
-if [ "$TRAVIS_BRANCH" = "1.7.x" ] && [ "$TRAVIS_PULL_REQUEST" = "false" ] && [[ "$TRAVIS_COMMIT_MESSAGE" == *"[ci deploy]"* ]]; then
+if [[ "$TRAVIS_COMMIT_MESSAGE" == *"[ci deploy]"* ]]; then
   mvn --settings .ci/settings.xml clean deploy -DskipTests -Pdeploy-parent -N
   mvn --settings .ci/settings.xml -f spring-cloud-zuul-ratelimit-dependencies/pom.xml clean deploy -DskipTests -Pdeploy
   mvn --settings .ci/settings.xml -f spring-cloud-starter-zuul-ratelimit/pom.xml clean deploy -DskipTests -Pdeploy
