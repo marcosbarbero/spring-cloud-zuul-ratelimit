b7bb5dd7de73669c3355c39fae0d6e6bd13ae282
Marcos Barbero <marcos.hgb@gmail.com>
2019-08-01 07:46:41 +0200

    Improve build messages [ci deploy]

--- .ci/deploy.sh
+++ .ci/deploy.sh
@@ -5,12 +5,14 @@ echo "Pull Request? $TRAVIS_PULL_REQUEST"
 echo "Commit Message: $TRAVIS_COMMIT_MESSAGE"
 
 if [[ "$TRAVIS_COMMIT_MESSAGE" == *"[ci deploy]"* ]]; then
+  echo "RELEASE INITIATED"
+
   mvn --settings .ci/settings.xml clean deploy -DskipTests -Pdeploy-parent -N
   mvn --settings .ci/settings.xml -f spring-cloud-zuul-ratelimit-dependencies/pom.xml clean deploy -DskipTests -Pdeploy
   mvn --settings .ci/settings.xml -f spring-cloud-starter-zuul-ratelimit/pom.xml clean deploy -DskipTests -Pdeploy
   mvn --settings .ci/settings.xml -f spring-cloud-zuul-ratelimit-core/pom.xml clean deploy -DskipTests -Pdeploy
 
-  echo "New version released"
+  echo "NEW VERSION RELEASED"
 fi
 
-echo "Build Finished"
+echo "BUILD FINISHED"
