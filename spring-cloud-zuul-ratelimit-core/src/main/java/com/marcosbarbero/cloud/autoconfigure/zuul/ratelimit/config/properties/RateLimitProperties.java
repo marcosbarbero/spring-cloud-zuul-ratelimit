01ffda0c264c82be21e4896a3d3a8b4be0453488
Marcos Barbero <marcos.hgb@gmail.com>
2019-09-18 11:16:23 +0200

    Remove unnecessary if statement

--- spring-cloud-zuul-ratelimit-core/src/main/java/com/marcosbarbero/cloud/autoconfigure/zuul/ratelimit/config/properties/RateLimitProperties.java
+++ spring-cloud-zuul-ratelimit-core/src/main/java/com/marcosbarbero/cloud/autoconfigure/zuul/ratelimit/config/properties/RateLimitProperties.java
@@ -76,9 +76,6 @@ public class RateLimitProperties {
     private int preFilterOrder = FORM_BODY_WRAPPER_FILTER_ORDER;
 
     public List<Policy> getPolicies(String key) {
-        if (StringUtils.isEmpty(key)) {
-            return defaultPolicyList;
-        }
         return policyList.getOrDefault(key, defaultPolicyList);
     }
 
