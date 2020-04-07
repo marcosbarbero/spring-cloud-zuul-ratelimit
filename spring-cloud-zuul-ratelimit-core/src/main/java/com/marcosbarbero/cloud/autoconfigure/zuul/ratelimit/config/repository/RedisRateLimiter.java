2f5f09486ff0843605007015c18297fd0952db2e
chenfang <fang.chen@drgou.com>
2020-04-07 14:53:30 +0800

    check & set expiration after increment operation to avoid in-expirable redis key

--- spring-cloud-zuul-ratelimit-core/src/main/java/com/marcosbarbero/cloud/autoconfigure/zuul/ratelimit/config/repository/RedisRateLimiter.java
+++ spring-cloud-zuul-ratelimit-core/src/main/java/com/marcosbarbero/cloud/autoconfigure/zuul/ratelimit/config/repository/RedisRateLimiter.java
@@ -67,6 +67,11 @@ public class RedisRateLimiter extends AbstractCacheRateLimiter {
             if (Boolean.FALSE.equals(present)) {
                 // Key already exists, increment
                 current = redisTemplate.opsForValue().increment(key, usage);
+                // in case the key went expired before increment, which will create a new key without expiration
+				// implicitly
+				if (redisTemplate.getExpire(key) == -1) {
+					redisTemplate.expire(key, refreshInterval.getSeconds(), SECONDS);
+				}
             } else {
                 current = usage;
             }
