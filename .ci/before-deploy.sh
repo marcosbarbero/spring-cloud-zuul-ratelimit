53275a3f168022d4fe6cc226fb088bf0349798cd
Marcos Barbero <marcos.hgb@gmail.com>
2021-10-22 11:55:26 +0200

    Add debuging message [ci deploy]

--- .ci/before-deploy.sh
+++ .ci/before-deploy.sh
@@ -1,6 +1,10 @@
 #!/usr/bin/env bash
 
+echo "[START GPG] Setup Signing Key"
+
 if [ "$TRAVIS_BRANCH" = "master" ] && [ "$TRAVIS_PULL_REQUEST" = "false" ] && [[ "$TRAVIS_COMMIT_MESSAGE" == *"[ci deploy]"* ]]; then
   openssl aes-256-cbc -K $encrypted_SOME_key -iv $encrypted_SOME_iv -in .ci/signingkey.asc.enc -out .ci/signingkey.asc -d
   gpg --fast-import .ci/signingkey.asc
 if
+
+echo "[END GPG] Setup Signing Key"
