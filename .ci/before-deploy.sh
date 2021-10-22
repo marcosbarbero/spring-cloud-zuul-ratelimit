b82a60a04889c9faa7883384f2feb4bf94367540
Marcos Barbero <marcos.hgb@gmail.com>
2021-10-22 11:40:19 +0200

    Fix GPG signing [ci deploy]

---
+++ .ci/before-deploy.sh
@@ -0,0 +1,6 @@
+#!/usr/bin/env bash
+
+if [ "$TRAVIS_BRANCH" = "master" ] && [ "$TRAVIS_PULL_REQUEST" = "false" ] && [[ "$TRAVIS_COMMIT_MESSAGE" == *"[ci deploy]"* ]]; then
+  openssl aes-256-cbc -K $encrypted_SOME_key -iv $encrypted_SOME_iv -in .ci/signingkey.asc.enc -out .ci/signingkey.asc -d
+  gpg --fast-import .ci/signingkey.asc
+if
