cd5ad030374056b066c3226d5c0afc78587cf9ce
Marcos Barbero <marcos.hgb@gmail.com>
2021-10-24 08:41:43 +0200

    Add gpg secret key location [ci deploy]

--- .ci/before-deploy.sh
+++ .ci/before-deploy.sh
@@ -1,9 +1,9 @@
-#!/usr/bin/env bash
+#!/usr/bin/env sh
 
 echo "[START GPG] Setup Signing Key"
 
 if [ "$TRAVIS_BRANCH" = "master" ] && [ "$TRAVIS_PULL_REQUEST" = "false" ] && [[ "$TRAVIS_COMMIT_MESSAGE" == *"[ci deploy]"* ]]; then
-  openssl aes-256-cbc -K $encrypted_01edcdfe89aa_key -iv $encrypted_01edcdfe89aa_iv -in .ci/secret.asc.enc -out .ci/secret.asc -d
+  - openssl aes-256-cbc -K $encrypted_01edcdfe89aa_key -iv $encrypted_01edcdfe89aa_iv -in .ci/secret.asc.enc -out .ci/secret.asc -d
 #  gpg --fast-import .ci/signingkey.asc
 if
 
