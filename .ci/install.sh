198efb8b6c58749ec40ef7714bab79e59abfebfe
Marcos Barbero <marcos.hgb@gmail.com>
2021-10-24 08:50:55 +0200

    Fix syntax [ci deploy]

--- .ci/install.sh
+++ .ci/install.sh
@@ -2,7 +2,7 @@
 
 echo "[START GPG] Setup Signing Key"
 
-if [ "$TRAVIS_BRANCH" = "master" ] && [ "$TRAVIS_PULL_REQUEST" = "false" ] && [[ "$TRAVIS_COMMIT_MESSAGE" == *"[ci deploy]"* ]];
+if [ "$TRAVIS_BRANCH" = "master" ] && [ "$TRAVIS_PULL_REQUEST" = "false" ] && [ "$TRAVIS_COMMIT_MESSAGE" == *"[ci deploy]"* ];
 then
   - openssl aes-256-cbc -K $encrypted_01edcdfe89aa_key -iv $encrypted_01edcdfe89aa_iv -in .ci/secret.asc.enc -out .ci/secret.asc -d
 fi
