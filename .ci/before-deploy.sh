6e4b3cd2f1dcac7a6cedb681d515ee7945897d43
Marcos Barbero <marcos.hgb@gmail.com>
2021-10-24 08:10:14 +0200

    Fix GPG encryption variables [ci deploy]

--- .ci/before-deploy.sh
+++ .ci/before-deploy.sh
@@ -3,8 +3,8 @@
 echo "[START GPG] Setup Signing Key"
 
 if [ "$TRAVIS_BRANCH" = "master" ] && [ "$TRAVIS_PULL_REQUEST" = "false" ] && [[ "$TRAVIS_COMMIT_MESSAGE" == *"[ci deploy]"* ]]; then
-  openssl aes-256-cbc -K $encrypted_SOME_key -iv $encrypted_SOME_iv -in .ci/signingkey.asc.enc -out .ci/signingkey.asc -d
-  gpg --fast-import .ci/signingkey.asc
+  openssl aes-256-cbc -K $encrypted_01edcdfe89aa_key -iv $encrypted_01edcdfe89aa_iv -in .ci/secret.asc.enc -out .ci/secret.asc -d
+#  gpg --fast-import .ci/signingkey.asc
 if
 
 echo "[END GPG] Setup Signing Key"
